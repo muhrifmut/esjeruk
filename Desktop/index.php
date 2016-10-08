db530e3de65688c4cccbbd4aebf4b6b939720287
Ghifari <ghifari226@gmail.com>
2016-10-08 19:50:04 +0700

    Menambahkan nim dan nama saya

--- Desktop/index.php
+++ Desktop/index.php
@@ -1,8 +1,6 @@
 <?php
 	echo "10114069 - M. Rifqi Muttaqin";
-<<<<<<< HEAD
 	echo "10114132 - Adi Saripuloh";
-=======
 	echo "10114415 - Muhammad Arief Saldy";
->>>>>>> d659a8c2e76d9b3e0054c36e6cbdbc1b6b96cdf1
+	echo "10114415 - Ghifari";
 ?>
