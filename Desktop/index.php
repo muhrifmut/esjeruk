167188ecbb93455e8cbb41fbc61ee871b0714782
Ghifari <ghifari226@gmail.com>
2016-10-08 19:51:08 +0700

    Revert "Menambahkan nim dan nama saya"
    This reverts commit db530e3de65688c4cccbbd4aebf4b6b939720287.

--- Desktop/index.php
+++ Desktop/index.php
@@ -1,6 +1,8 @@
 <?php
 	echo "10114069 - M. Rifqi Muttaqin";
+<<<<<<< HEAD
 	echo "10114132 - Adi Saripuloh";
+=======
 	echo "10114415 - Muhammad Arief Saldy";
-	echo "10114415 - Ghifari";
+>>>>>>> d659a8c2e76d9b3e0054c36e6cbdbc1b6b96cdf1
 ?>
